1f0c35510f14ab4ea74675359d1324a3a76a9c40
peakhunt <hawk.kim1974@gmail.com>
2021-03-05 09:40:33 +0900

    better simpler motion control demo

--- app/motion_demo.c
+++ app/motion_demo.c
@@ -11,19 +11,16 @@
 
 typedef enum
 {
-  motion_demo_state_wait1,
-  motion_demo_state_fwd,
-  motion_demo_state_wait2,
-  motion_demo_state_bwd,
-  motion_demo_state_wait3,
-  motion_demo_state_fwd_bwd,
-} motion_demo_state_t;
+  motion_command_forward,
+  motion_command_backward,
+  motion_command_wait,
+} motion_command_t;
 
-typedef enum
+typedef struct
 {
-  motion_demo_event_timeout,
-  motion_demo_event_step_complete,
-} motion_demo_event_t;
+  motion_command_t      cmd;
+  uint32_t              arg;
+} motion_command_struct_t;
 
 //
 // I'm using 0.9 degree stepper.
@@ -33,237 +30,90 @@ typedef enum
 // 0 means oen step
 // 200 means 199 step
 //
-static uint8_t    _fwd_rotate[] =
+static motion_command_struct_t    _commands[] =
 {
-  0,
-  0,
-  197,
-  55,
-  3,
-  7,
-  29,
-  33,
-  67,
-};
+  { motion_command_wait,      2000 },
 
-static uint8_t    _bwd_rotate[] =
-{
-  77,
-  77,
-  3,
-  0,
-  1,
-  2,
-  3,
-  99,
-  129,
-};
+  { motion_command_forward,   0 },
+  { motion_command_forward,   0 },
+  { motion_command_forward,   197 },
+  { motion_command_forward,   55 },
+  { motion_command_forward,   3 },
+  { motion_command_forward,   7 },
+  { motion_command_forward,   29 },
+  { motion_command_forward,   33 },
+  { motion_command_forward,   67 },
 
-static int8_t _fwd_bwd_test[] =
-{
-  -30,
-   30,
-   30,
-  -30,
+  { motion_command_wait,      500 },
 
-  -40,
-   40,
-   40,
-  -40,
+  { motion_command_backward,  77 },
+  { motion_command_backward,  77 },
+  { motion_command_backward,  3 },
+  { motion_command_backward,  0 },
+  { motion_command_backward,  1 },
+  { motion_command_backward,  2 },
+  { motion_command_backward,  3 },
+  { motion_command_backward,  99 },
+  { motion_command_backward,  129 },
 
-  -50,
-   50,
-   50,
-  -50,
+  { motion_command_wait,      500 },
 
-  -60,
-   60,
-   60,
-  -60,
+  { motion_command_backward,  30 },
+  { motion_command_forward,   30 },
+  { motion_command_forward,   30 },
+  { motion_command_backward,  30 },
 
-  -70,
-   70,
-   70,
-  -70,
+  { motion_command_backward,  40 },
+  { motion_command_forward,   40 },
+  { motion_command_forward,   40 },
+  { motion_command_backward,  40 },
 
-  -80,
-   80,
-   80,
-  -80,
+  { motion_command_backward,  50 },
+  { motion_command_forward,   50 },
+  { motion_command_forward,   50 },
+  { motion_command_backward,  50 },
 
-  -90,
-   90,
-   90,
-  -90
+  { motion_command_backward,  60 },
+  { motion_command_forward,   60 },
+  { motion_command_forward,   60 },
+  { motion_command_backward,  60 },
+
+  { motion_command_backward,  70 },
+  { motion_command_forward,   70 },
+  { motion_command_forward,   70 },
+  { motion_command_backward,  70 },
+
+  { motion_command_backward,  80 },
+  { motion_command_forward,   80 },
+  { motion_command_forward,   80 },
+  { motion_command_backward,  80 },
+
+  { motion_command_backward,  90 },
+  { motion_command_forward,   90 },
+  { motion_command_forward,   90 },
+  { motion_command_backward,  90 },
 };
 
 static SoftTimerElem          _tmr;
-static motion_demo_state_t    _state;
 static uint32_t               _current_ndx;
 
 static void
-execute_step(void)
+execute_command(void)
 {
-  switch(_state)
+  motion_command_struct_t*    cmd = &_commands[_current_ndx];
+
+  switch(cmd->cmd)
   {
-  case motion_demo_state_fwd:
-    stepper_step(true, _fwd_rotate[_current_ndx]);
+  case motion_command_forward:
+    stepper_step(true, (uint8_t)cmd->arg);
     break;
 
-  case motion_demo_state_bwd:
-    stepper_step(false, _bwd_rotate[_current_ndx]);
+  case motion_command_backward:
+    stepper_step(false, (uint8_t)cmd->arg);
     break;
 
-  case motion_demo_state_fwd_bwd:
-    {
-      int16_t step = _fwd_bwd_test[_current_ndx];
-
-      if(step < 0)
-      {
-        step *= -1;
-        stepper_step(false, (uint8_t)(step));
-      }
-      else
-      {
-        stepper_step(true, (uint8_t)(step));
-      }
-    }
-    break;
-
-  default:
-    break;
-  }
-}
-
-static void
-enter_state(motion_demo_state_t new_state)
-{
-  _state = new_state;
-
-  switch(_state)
-  {
-  case motion_demo_state_wait1:
-    mainloop_timer_schedule(&_tmr, IDLE_TIME);
-    break;
-
-  case motion_demo_state_fwd:
-    _current_ndx = 0;
-    execute_step();
-    break;
-
-  case motion_demo_state_wait2:
-    mainloop_timer_schedule(&_tmr, BWD_WAIT_TIME);
-    break;
-
-  case motion_demo_state_bwd:
-    _current_ndx = 0;
-    execute_step();
-    break;
-
-  case motion_demo_state_wait3:
-    mainloop_timer_schedule(&_tmr, BWD_WAIT_TIME);
-    break;
-
-  case motion_demo_state_fwd_bwd:
-    _current_ndx = 0;
-    execute_step();
-    break;
-  }
-}
-
-static void
-motion_demo_event_handler(motion_demo_event_t e)
-{
-  switch(_state)
-  {
-  case motion_demo_state_wait1:
-    if(e == motion_demo_event_timeout)
-    {
-      enter_state(motion_demo_state_fwd);
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      // impossible. BUG if occurs
-    }
-    break;
-
-  case motion_demo_state_fwd:
-    if(e == motion_demo_event_timeout)
-    {
-      // impossible. BUG if occurs
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      _current_ndx++;
-      if(_current_ndx < sizeof(_fwd_rotate))
-      {
-        execute_step();
-      }
-      else
-      {
-        enter_state(motion_demo_state_wait2);
-      }
-    }
-    break;
-
-  case motion_demo_state_wait2:
-    if(e == motion_demo_event_timeout)
-    {
-      enter_state(motion_demo_state_bwd);
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      // impossible. BUG if occurs
-    }
-    break;
-
-  case motion_demo_state_bwd:
-    if(e == motion_demo_event_timeout)
-    {
-      // impossible. BUG if occurs
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      _current_ndx++;
-      if(_current_ndx < sizeof(_bwd_rotate))
-      {
-        execute_step();
-      }
-      else
-      {
-        enter_state(motion_demo_state_wait3);
-      }
-    }
-    break;
-
-  case motion_demo_state_wait3:
-    if(e == motion_demo_event_timeout)
-    {
-      enter_state(motion_demo_state_fwd_bwd);
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      // impossible. BUG if occurs
-    }
-    break;
-
-  case motion_demo_state_fwd_bwd:
-    if(e == motion_demo_event_timeout)
-    {
-      // impossible. BUG if occurs
-    }
-    else if(e == motion_demo_event_step_complete)
-    {
-      _current_ndx++;
-      if(_current_ndx < sizeof(_fwd_bwd_test))
-      {
-        execute_step();
-      }
-      else
-      {
-        enter_state(motion_demo_state_wait1);
-      }
-    }
+  case motion_command_wait:
+    mainloop_timer_schedule(&_tmr, cmd->arg);
     break;
   }
 }
@@ -271,13 +121,18 @@ motion_demo_event_handler(motion_demo_event_t e)
 static void
 stepper_complete_handler(uint32_t e)
 {
-  motion_demo_event_handler(motion_demo_event_step_complete);
+  _current_ndx++;
+  if(_current_ndx >= sizeof(_commands)/sizeof(motion_command_struct_t))
+  {
+    _current_ndx = 0;
+  }
+  execute_command();
 }
 
 static void
 motion_demo_timeout(SoftTimerElem* te)
 {
-  motion_demo_event_handler(motion_demo_event_timeout);
+  stepper_complete_handler(0);
 }
 
 void
@@ -288,5 +143,6 @@ motion_demo_init(void)
 
   event_register_handler(stepper_complete_handler, DISPATCH_EVENT_STEPPER_COMPLETE);
 
-  enter_state(motion_demo_state_wait1);
+  _current_ndx = 0;
+  execute_command();
 }
